adc834d63f40a006a16590bae60ce9945fbef966
xuen012 <xuen998@gmail.com>
2026-09-13 19:58:02 +0800

    add comment

--- model/config.py
+++ model/config.py
@@ -14,6 +14,7 @@ class train_config(BaseModel):
 
 config2 = train_config(train_batch_size=32, num_epochs=2000, lr_warmup_steps=50, save_model_epochs=200, num_train_timesteps=1000, print_epoch_loss=50)
 
+# keep track on timestamps for step by step noising process to input in model
 time_embed_dim = 64
 time_proj = Timesteps(
                 time_embed_dim, flip_sin_to_cos=True, downscale_freq_shift=0.0
