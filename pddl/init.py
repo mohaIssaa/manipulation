babcc5d577c98e6cae37e378390cb3df072f650e
xuen012 <xuen998@gmail.com>
2026-09-13 20:00:32 +0800

    add comments

--- pddl/init.py
+++ pddl/init.py
@@ -1,4 +1,4 @@
-# loaded model architecture
+# load model architecture
 time_embed_dim = 64
 noise_scheduler = DDIMScheduler(num_train_timesteps=1000, prediction_type="sample")
 loaded_model = unet(grasp_dim=17, cond_dim=10, mid_dim=64, time_dim=64)
@@ -13,7 +13,6 @@ noise_scheduler.set_timesteps(num_inference_steps=1000)
 num_total = 4
 grasp_dim = 17; cond_dim = 10; mid_dim=64; time_dim=64
 test_batch_size = 1
-# 給定你要測試的新條件 (B, 9, N)
-test_block_cnn = test_block_info.unsqueeze(0)#torch.randn(batch_size, block_dim, num_total)
+test_block_cnn = test_block_info.unsqueeze(0)
 uncond_block_cnn = torch.zeros_like(test_block_cnn)
 generated_grasp_cnn = torch.randn(test_batch_size, grasp_dim, num_total)
