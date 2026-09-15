7fa5232f9859568d5bcaca99ebeaabfcb7bdaa82
xuen012 <xuen998@gmail.com>
2026-09-15 14:04:45 +0800

    with data collection with obstacle placed

--- data/collect_data.py
+++ data/collect_data.py
@@ -2,18 +2,13 @@ from ../env/builder.py import make_builder
 import numpy as np
 from tampanda.symbolic.domains.blocks.blocks_domain import BlocksDomain
 
-def collect_data(n=5):
+def collision_check(x, px, y, py, w, pw, d, pd, clearance):
+    return (abs(x - px) < (w + pw) / 2 + clearance and abs(y - py) < (d + pd) / 2 + clearance)
+def collect_data(n):
 
     obj_list = []
     for i in range(n):
-        builder = make_builder()
-
-        # sampling
-        rng = np.random.default_rng(seed)
-        x = rng.uniform(0.30, 0.65)
-        y = rng.uniform(-0.20, 0.20)
-        pose = [x, y, 0.27]
-
+     
         # 1,2: default block with size 4, and 6. 3-6: blocks from ycb for extended advanced test.
         """
         Name ID weights dims(mm)
@@ -25,42 +20,98 @@ def collect_data(n=5):
         bricks: 60 Foam Brick 28g 50 x 75 x 50
         69 Colored Wood Blocks 10.8g 26
         """
-        idx = np.random.choice(np.arange(6), p=[0.4, 0.4, 0.05, 0.05, 0.08, 0.02]) # 0.28, 0.28, 0.15, 0.15, 0.02, 0.10, 0.02
+
+        builder = make_builder()
+
+        # sampling
+        rng = np.random.default_rng()
+        margin = 0.05
+        
+        #num_obstacles = np.randint(2,3)
+        #for n in range(num_obstacles):
+        obs_1_x = rng.uniform(0.15 + margin, 0.55 - margin)
+        obs_1_y = rng.uniform(0.20 + margin, 0.60 - margin)
+        obs_pose_1 = [obs_1_x, obs_1_y, 0.27]
+        obs_idx_1 = np.random.choice(np.arange(6), p=[0.5, 0.5, 0.0, 0.0, 0.0, 0.0])
+        
+        obs_2_x = rng.uniform(0.15 + margin, 0.55 - margin)
+        obs_2_y = rng.uniform(0.20 + margin, 0.60 - margin)
+        obs_pose_2 = [obs_2_x, obs_2_y, 0.27]
+        obs_idx_2 = np.random.choice(np.arange(6), p=[0.5, 0.5, 0.0, 0.0, 0.0, 0.0])
+
+        obs_list = [obs_pose_1, obs_pose_2]
+        obs_idx = [obs_idx_1, obs_idx_2]
+        builder.add_object("cube", pos=obs_pose_1, name="obs_a")
+
+        x = rng.uniform(0.15 + margin, 0.55 - margin)
+        y = rng.uniform(0.20 + margin, 0.60 - margin)
+        pose = [x, y, 0.27]
+        
+        idx = np.random.choice(np.arange(6), p=[0.5, 0.5, 0.0, 0.0, 0.0, 0.0]) # 0.28, 0.28, 0.15, 0.15, 0.02, 0.10, 0.02
         if idx == 0:
             builder.add_object("cube", pos=pose, name="block_a")
+            shape = [0.04, 0.04, 0.04]
         elif idx == 1:
             builder.add_object("block", pos=pose, name="block_a")
+            shape = [0.06, 0.06, 0.06]
         elif idx == 2:
             builder.add_object("pudding_box", pos=pose, name="block_a")
+            shape = [0, 0, 0]
         elif idx == 3:
             builder.add_object("gelatin_box", pos=pose, name="block_a")
+            shape = [0, 0, 0]
         elif idx == 4:
             builder.add_object("brick", pos=pose, name="block_a")
+            shape = [0, 0, 0]
         elif idx == 5:
             builder.add_object("wood_block", pos=pose, name="block_a")
-
-        # set block position within work space
+            shape = [0, 0, 0]
+        
         env = builder.build_env(rate=200.0)
+        
+        print(env.get_object_pose("block_a"))
+        
         domain  = BlocksDomain(env.model, table_geom_name="table_surface")
-        BOUNDS  = domain.get_working_bounds()        
-        BLOCK_HALF = env.get_object_half_size("block_a")
-        env.set_object_pose("block_a", np.array([x, y, BOUNDS["table_height"] + BLOCK_HALF[2] + 0.003]))
-        env.reset_velocities(); env.forward(); env.rest(0.4)
+        BOUNDS  = domain.get_working_bounds()
+        #print(BOUNDS)
 
-        # obtain candidate data
+        BLOCK_HALF = env.get_object_half_size("block_a")
+        
+        # check collision obs_1 and block
+        if obs_idx_1 == 0:
+            obs_width_1 = 0.04
+            obs_depth_1 = 0.04
+        else:
+            obs_width_1 = 0.06
+            obs_depth_1 = 0.06
+        #if obs_idx_2 == 0:
+        #    obs_width_2 = 0.04
+        #    obs_depth_2 = 0.04
+        #else:
+        #    obs_width_2 = 0.06
+        #    obs_depth_2 = 0.06
+        clearance = 0.01
+        collision = collision_check(pose[0],obs_pose_1[0],pose[1],obs_pose_1[1],shape[0],obs_width_1,shape[1],obs_depth_1,clearance)
+        print(collision)
+        if collision:
+            set_pose = sampling(BOUNDS["min_x"],BOUNDS["max_x"],BOUNDS["min_y"],BOUNDS["max_y"], margin)
+            env.set_object_pose("block_a", np.array([set_pose[0], set_pose[1], BOUNDS["table_height"] + BLOCK_HALF[2] + 0.003]))
+            env.reset_velocities(); env.forward(); env.rest(0.4)
+
+        print(builder._get_object_pos("block_a"))
+        #print("world:",builder._resolve_world_poses())
+
+        print(env.get_object_pose("block_a"))
+
         grasp_planner = GraspPlanner(table_z=0.00)
-        pos  = env.get_object_position("block_a")
+        pos  = env.get_object_pose("block_a")[0]
         half = env.get_object_half_size("block_a")
-        quat = env.get_object_orientation("block_a")
+        quat = env.get_object_pose("block_a")[1]
         candidates = grasp_planner.generate_candidates(pos, half, quat)
 
-        obj_list.append([pos, half, quat, candidates])
+        obj_list.append([pos, half, quat, candidates, shape])
 
-        print(f"working area x ∈ [{BOUNDS['min_x']:.2f},{BOUNDS['max_x']:.2f}], "
-        f"y ∈ [{BOUNDS['min_y']:.2f},{BOUNDS['max_y']:.2f}], table_z={BOUNDS['table_height']:.3f}")
-
-        show_views(env)
-    return obj_list
+    return obj_list, builder, env, obs_list, obs_idx
 
 if __name__ == "__main__":
     print(collect_data())
